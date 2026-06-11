39890e7026a4c0f6a227e3cf06368e06de878bc5
christopheBomfim <christopherbonfim498@gmail.com>
2026-06-10 22:53:55 -0300

    FUNCIONA POR FAVOR

--- blackjack/src/main/java/servlet/IniciarJogoServlet.java
+++ blackjack/src/main/java/servlet/IniciarJogoServlet.java
@@ -2,14 +2,13 @@ package servlet;
 
 import java.io.IOException;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import model.GerenciadorJogos;
 
-@WebServlet("/iniciar")
+
 public class IniciarJogoServlet extends HttpServlet {
 
 	@Override
